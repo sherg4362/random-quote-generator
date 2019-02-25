26dfdda47f33525f80c5250a00b2a16214cfe89d
sherg <sgamble284.gmail.com>
2019-02-24 19:23:27 -0500

    Added function to generate random number, used to select quote from array

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ const quotes = [
     source: "Dalai Lama"
   },
   {
-    quote: "We may encounter many defeats but we must not be defeated."
+    quote: "We may encounter many defeats but we must not be defeated.",
     source: "Maya Angelou"
   }
 ];
@@ -47,31 +47,35 @@ const quotes = [
    - use the random number to `return` a random quote object from the 
      `quotes` array.
 ***/
+// Generates a random number, returns a quote from array
+function getRandomQuote() {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+};
 
-
+getRandomQuote();
 
 
 /***
-  Create the `printQuote` function to: 
+  Create the `printQuote` function to:
    - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
+   - use the properties of the quote object stored in the variable to
      create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
+   - use conditionals to make sure the optional properties exist before
      they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. 
+   - set the `innerHTML` of the `quote-box` div to the HTML string.
 ***/
 
 
 
 
 /***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
+  When the "Show another quote" button is clicked, the event listener
+  below will be triggered, and it will call, or "invoke", the `printQuote`
+  function. So do not make any changes to the line of code below this
   comment.
 ***/
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
